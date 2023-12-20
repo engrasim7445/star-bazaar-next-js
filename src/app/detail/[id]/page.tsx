19ba4cef988d8361dac9c73c6c26d46a7826ce80
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2023-12-20 17:26:36 +0500

    Creator Detail Page

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -3,14 +3,11 @@ import { ContainerA } from '@/theme';
 
 export default function BusinessComponent() {
   return (
-    <>
-      <ContainerA>
-        <div className="col-span-12">
-          <div className="grid grid-cols-6 gap-1 md:gap-2 xl:gap-4">
-           
-          </div>
-        </div>
-      </ContainerA>
-    </>
+    <ContainerA>
+      <div className="col-span-12">
+        <div className="grid grid-cols-6 gap-1 md:gap-2 xl:gap-4"></div>
+      </div>
+      <Testamonial />
+    </ContainerA>
   );
 }
